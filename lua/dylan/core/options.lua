56daee2a3b823c02746b61162e218a8ae06ab6e9
Dylan Booth <dbooth05@iastate.edu>
2025-03-28 12:44:07 -0500

    fix diffview error

--- lua/dylan/core/options.lua
+++ lua/dylan/core/options.lua
@@ -1,5 +1,17 @@
 local opt = vim.o
 
+-- Remove trailing spaces automatically on BufEnter
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*",  -- This will apply to all buffers
+  command = "silent! %s/\\s\\+$//e",  -- Removes trailing spaces
+})
+
+-- Remove `o` from formatoptions on BufEnter
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*",  -- This will apply to all buffers
+  command = "setlocal formatoptions-=o",  -- Removes 'o' from formatoptions
+})
+
 opt.autoindent = true  	-- copy indent from the previous line
 opt.smartindent = true 	-- auto add extra indent
 opt.expandtab = true   	-- use spaces instead of tabs
@@ -8,5 +20,4 @@ opt.shiftwidth = 4 	-- number of spaces per indent level
 opt.tabstop = 4		-- number of spaces that a tab counts for
 opt.softtabstop = 4	-- helps maintain spaces when pressing tab
 
-vim.cmd([[ autocmd BufEnter * %s/\s\+$//e ]]) -- remove trailing white space auto
-vim.cmd([[ autocmd BufEnter * setlocal formatoptions-=o ]]) -- fix \n behavior
+
